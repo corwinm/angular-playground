3f35542e55e13792aac91c35a431d724ffdcef99
Corwin Marsh <corwin@corwinsmacbook.local>
2018-06-24 10:26:39 -0700

    Basic grid layout and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PictureGridComponent } from './picture-grid/picture-grid.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PictureGridComponent,
+    NavigationBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/picture-grid/picture-grid.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pg-picture-grid',
+  templateUrl: './picture-grid.component.html',
+  styleUrls: ['./picture-grid.component.scss']
+})
+export class PictureGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
