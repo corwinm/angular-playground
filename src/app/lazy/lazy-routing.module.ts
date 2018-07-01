cc0e789db8bb987909ab28b07468fd0cef968fe9
Corwin Marsh <corwin@corwinsmacbook.local>
2018-06-30 20:21:40 -0700

    Cleanup

--- src/app/lazy/lazy-routing.module.ts
+++ src/app/lazy/lazy-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PictureGridComponent } from '../picture-grid/picture-grid.component';
+import { PictureGridComponent } from './picture-grid/picture-grid.component';
 
 const routes: Routes = [
   { path: '', component: PictureGridComponent }
--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LazyRoutingModule } from './lazy-routing.module';
-import { PictureGridComponent } from '../picture-grid/picture-grid.component';
+import { PictureGridComponent } from './picture-grid/picture-grid.component';
 
 @NgModule({
   imports: [
