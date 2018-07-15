9cac28195b0f3dceda8ac0b72e1d0efab66c8967
Corwin Marsh <corwin@corwinsmacbook.local>
2018-07-14 22:15:01 -0700

    Refactor and enable root scss imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
-import { NavigationItemComponent } from './navigation-item/navigation-item.component';
 import { HomeComponent } from './home/home.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { NavigationBarComponent } from './navigation/navigation-bar/navigation-bar.component';
+import { NavigationMenuComponent } from './navigation/navigation-menu/navigation-menu.component';
+import { NavigationItemComponent } from './navigation/navigation-item/navigation-item.component';
 
 @NgModule({
   declarations: [
