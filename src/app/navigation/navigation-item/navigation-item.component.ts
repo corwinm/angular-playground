63426ac85e3cdc748c47783069650fc587e684e6
Corwin Marsh <corwin@corwinsmacbook.local>
2018-07-29 21:52:34 -0700

    Cleanup

--- src/app/navigation/navigation-item/navigation-item.component.ts
+++ src/app/navigation/navigation-item/navigation-item.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'pg-navigation-item',
   templateUrl: './navigation-item.component.html',
   styleUrls: ['./navigation-item.component.scss']
 })
-export class NavigationItemComponent implements OnInit {
+export class NavigationItemComponent {
 
   @Input() route: String;
   @Input() label: String;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
